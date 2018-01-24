9e777e5e56709a9679cc85f113d486c4e9112c6f
kongxiaoan <18153976553@163.com>
2018-01-24 15:47:22 +0800

    init project

---
+++ src/main/java/com/kpa/IWillPay/IWillPayApplication.java
@@ -0,0 +1,12 @@
+package com.kpa.IWillPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IWillPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IWillPayApplication.class, args);
+	}
+}
